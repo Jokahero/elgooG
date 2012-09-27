2ed85ad1fe18cfede58066936d075bfcbefc7d6f
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-09-27 14:21:23 +0200

    Connexion à la base de données (fixes #4)

--- src/launcher.rb
+++ src/launcher.rb
@@ -3,6 +3,9 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'parser'
+require 'mysql'
+
+db = nil
 
 def lookForFiles(path)
 	Dir.foreach(path) do |file|
@@ -15,7 +18,26 @@ def lookForFiles(path)
 	end
 end
 
+def connectToDatabase(host='localhost', user='root', password='root', database='elgoogaa')
+	begin
+		db = Mysql.new
+		db.connect(host, user, password, database)
+	rescue Mysql::Error => e
+		puts e
+		return false
+	end
+	return true
+end
+
+if not connectToDatabase then
+	puts "Unable to establish connection to the database!"
+	exit 1
+end
+
 ARGV.each do|path|
 	puts "Looking for documents in #{path}…"
 	lookForFiles(path)
 end
+
+db.close
+
