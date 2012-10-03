7a3a9a32fff59e34046cde16dee3ee5044eb7fe2
Nicolas Buffon <akara@pixelized.fr>
2012-10-03 17:12:15 +0200

    Modification de la méthode getWeight.

--- src/wordinfo.rb
+++ src/wordinfo.rb
@@ -4,45 +4,45 @@ require './src/formulas.rb'
 
 class WordInfo
 
-@@TITLE_WEIGHT = 3 
-@@UNDER_TITLE_WEIGHT = 2
+	@@TITLE_WEIGHT = 3 
+	@@UNDER_TITLE_WEIGHT = 2
 
-attr_accessor :document, :xPathList, :weight, :positions
+	attr_accessor :document, :xPathList, :weight, :positions
 
-def initialize
-	@xPathList = Hash.new
-	@weight = 0
-end
+	def initialize
+		@xPathList = Hash.new
+		@weight = 0
+	end
 
-def addParagraph(xPath)
-	@xPathList << xPath
-end
+	def addParagraph(xPath)
+		@xPathList << xPath
+	end
 
-def addTitleWeight
-	@weight = @weight + @@TITLE_WEIGHT
-end
+	def addTitleWeight
+		@weight = @weight + @@TITLE_WEIGHT
+	end
 
-def addUnderTitleWeight
-	@weight = @weight + @@UNDER_TITLE_WEIGHT
-end
+	def addUnderTitleWeight
+		@weight = @weight + @@UNDER_TITLE_WEIGHT
+	end
 
-def addOccurence(xPath, position)
-	positions = @xPathList[xPath]
-	@xPathList[xPath] = Array.new if positions == nil
-	@xPathList[xPath] << position
-end
+	def addOccurence(xPath, position)
+		positions = @xPathList[xPath]
+		@xPathList[xPath] = Array.new if positions == nil
+		@xPathList[xPath] << position
+	end
 
-def getFrequency(xPath)
-	return @xPathList[xPath].length
-end
+	def getFrequency(xPath)
+		return @xPathList[xPath].length
+	end
 
-def eachPath(&block)
-	@xPathList.each(&block)
-end
+	def eachPath(&block)
+		@xPathList.each(&block)
+	end
 
-def getWeight(length, xPath)
-	return computeTF(length, getFrequency(xPath)) + @weight
-end
-
+	def getWeight(length, xPath, paragraphCount)
+		#return computeTF(length, getFrequency(xPath)) * computeIDF(paragraphCount, @xPathList.length) +  @weight
+		return getFrequency(xPath) * computeIDF(paragraphCount, @xPathList.length) +  @weight
+	end
 end # WordInfo
 
