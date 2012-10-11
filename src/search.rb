d8e527abe92424c625e83fd8cfa3cb2f10111672
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-10-11 21:10:20 +0200

    Recherche des paragraphes communs

--- src/search.rb
+++ src/search.rb
@@ -4,20 +4,29 @@ $LOAD_PATH << File.dirname(__FILE__)
 
 require 'db'
 
-def searchPattern(pattern)
-	checkDatabaseConnection
-	terms = pattern.split(/[^a-zA-ZàÀâÂéÉèÈêÊçÇîÎôÔûÛ]/)
-	infos = {}
-	# Lookup each word
-	terms.each {|t|
-		infos[t] = searchWord t
-	}
-	$db.close if $db != nil
+class Term
+	attr_accessor :term, :found, :next
 
+	def initialize(string)
+		terms = string.split(/[^a-zA-ZàÀâÂéÉèÈêÊçÇîÎôÔûÛ]/)
+		@term = terms[0]
+		@found = searchWord @term
+		if terms.size > 1 then
+			@next = Term.new(terms[1..-1].join(" "))
+		else
+			@next = nil
+		end
+	end
+end
+
+def searchPattern(pattern)
+	root = Term.new(pattern)
+	# Search commons paragraphs
+	common = checkCommonParagraphs(root)
+	puts common
 end
 
 def searchWord(word)
-	checkDatabaseConnection
 	res = $db.query("SELECT `Found`.`id`, `Found`.`paragraph`, `Found`.`weight` FROM `Found`, `Words` WHERE `Words`.`word` = '#{word}' AND `Words`.`id` = `Found`.`word`;")
 	found = {}
 	while row = res.fetch_hash do
@@ -36,5 +45,29 @@ def searchWord(word)
 	return found
 end
 
+def checkCommonParagraphs(start)
+	accu = []
+	start.found.each_key {|p|
+		accu << p
+	}
+	checkCommonParagraphsAux(start, accu)
+end
+
+def checkCommonParagraphsAux(start, accu)
+	accu.each {|p|
+		if not start.found.has_key? p then
+			accu.delete p
+		end
+	}
+
+	if start.next != nil then
+		checkCommonParagraphsAux(start.next, accu)
+	else
+		return accu
+	end
+end
+
+checkDatabaseConnection
 searchPattern("village randonnée montagne asie")
+$db.close if $db != nil
 
