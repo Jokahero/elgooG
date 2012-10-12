d40c9eb0bdae41ba1cf7c58667eb4ec3cd4d2f8e
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-10-12 14:58:30 +0200

    Recherche des différentes requêtes de test

--- tests/queries.rb
+++ tests/queries.rb
@@ -1,19 +1,38 @@
 #coding : utf-8
 
-$LOAD_PATH << File.dirname(__FILE__)
+$LOAD_PATH << File.dirname(__FILE__) + '/../src'
 
-require 'rexml/ocumnt'
+require 'db'
+require 'search'
+
+require 'rexml/document'
 
 include REXML
 
+QUERY = 'query'
+TEXT = 'text'
+NARRATIVE = 'narrative'
+
 file = File.new('tests/queries.xml')
-document = Docment.new(file)
+document = Document.new(file)
 
 root = document.root
 return if not root.name == 'queries'
 
+checkDatabaseConnection
+
 # Parcours des requêtes
 root.each_element do |element|
-
+	if element.name == QUERY then
+		currentQuery = element.attributes['id']
+		element.each_element do |elt|
+			if elt.name == TEXT then
+				puts elt.text
+				qrel = searchPattern(elt.text)
+			end
+		end
+	end
 end
+
+$db.close if $db != nil
 
